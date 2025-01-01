1fcb1490a4bf6024738a24fcf9e6808b2ad9667c
hridoykhan51 <hridoykhan6786@gmail.com>
2025-01-01 20:17:34 +0600

    final commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,82 +6,202 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Stack(children: [
-            Container(
-              height: 200,
-              width: double.infinity,
+          Column(
+            children: [
+              Stack(children: [
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(300),
+                    ),
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(fontSize: 30, color: Colors.white),
+                        ),
+                        Text(
+                          'Login to your account',
+                          style: TextStyle(fontSize: 20, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  left: -110,
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(175, 78, 78, 137),
+                        borderRadius: BorderRadius.circular(70)),
+                  ),
+                )
+              ]),
+              SizedBox(
+                height: 50,
+              ),
+              Card(
+                color: Colors.blue[200],
+                elevation: 7,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Checkbox(value: false, onChanged: (value) {}),
+                              Text('Remember me'),
+                            ],
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Text('Login'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Forgot password?'),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('Reset'),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                      indent: 10,
+                      endIndent: 10,
+                    ),
+                  ),
+                  Text('Or login with'),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                      thickness: 1,
+                      indent: 10,
+                      endIndent: 10,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.blue,
+                    child: Icon(
+                      Icons.facebook,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.blue,
+                    child: Icon(
+                      Icons.email,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.blue,
+                    child: Icon(Icons.phone, color: Colors.white),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Container(
+              height: 100,
+              width: 300,
               decoration: BoxDecoration(
                 color: Colors.blue,
                 borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(300),
+                  topLeft: Radius.circular(300),
                 ),
               ),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Welcome back',
-                      style: TextStyle(fontSize: 30, color: Colors.white),
-                    ),
-                    Text(
-                      'Login to your account',
-                      style: TextStyle(fontSize: 20, color: Colors.black),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Positioned(
-              left: -110,
-              child: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                    color: const Color.fromARGB(175, 78, 78, 137),
-                    borderRadius: BorderRadius.circular(70)),
-              ),
-            )
-          ]),
-          SizedBox(
-            height: 70,
-          ),
-          Card(
-            color: Colors.blue[200],
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      hintText: 'Email',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+                  Center(
+                    child: Text(
+                      'Create an account?',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Password',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Login'),
-                  ),
+                  TextButton(
+                      autofocus: true,
+                      onPressed: () {},
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(color: Colors.black),
+                      ))
                 ],
               ),
             ),
